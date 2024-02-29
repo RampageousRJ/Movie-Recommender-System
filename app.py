2c6b0f9b6eacc801293405d57c1c87b813d942f1
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2024-02-29 12:49:06 +0530

    Flask Frontend

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask,request
+from flask import Flask,request,render_template,url_for
+from wtforms import StringField,SubmitField
+from flask_wtf import FlaskForm
 import pandas as pd
 import pickle
+import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='supersecretspecialkey'
@@ -13,23 +16,38 @@ fm = open('model/matrix.pkl','rb')
 similarity_matrix = pickle.load(fm)
 fm.close()
 
+class MovieForm(FlaskForm):
+    title = StringField("Enter movie: ")
+    submit = SubmitField("Recommend")
+
 def fetch(movie):
     try:
         index = df[df['Title_Lower']==movie.lower()].index[0]
     except:
         return "Not Available"
-    movies = sorted(list(enumerate(similarity_matrix[index])),reverse=True,key=lambda x:x[1])[1:6]
+    movies = sorted(list(enumerate(similarity_matrix[index])),reverse=True,key=lambda x:x[1])[1:7]
     similar_movies = []
     for movie in movies:
         similar_movies.append(df.iloc[movie[0]])
-    return pd.DataFrame(similar_movies).sort_values(by='Popularity',ascending=False).drop(['Title_Lower','Overview_Stemmed'],axis=1).to_json()
+    return pd.DataFrame(similar_movies).sort_values(by='Popularity',ascending=False).drop(['Title_Lower','Overview_Stemmed'],axis=1).T.to_json()
 
 @app.route('/api',methods=['POST'])
 def api():
     payload = request.get_json()
     result = fetch(payload['title'])
-    print(result)
     return result
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    movies = []
+    form = MovieForm()
+    if request.method == 'POST':
+        data = {'title':form.title.data}
+        response = requests.post("http://127.0.0.1:5000/api",json=data).json()
+        for row in response:
+            movies.append(response[row]['Title'])
+        return render_template('home.html',movies=movies,form=form)
+    return render_template('home.html',form=form)
 
 if __name__=='__main__':
     app.run(debug=1)
