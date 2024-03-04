a09bdd171bb34a61506b7034a9a78d54d3cda6d1
Swapnil Jha <swapnil.strange0707@gmail.com>
2024-03-04 22:24:36 +0530

    Home page content and animations updated

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import './home.css'
 import svg from '../../../src/assets/undraw_search_app_oso2.svg'
@@ -7,9 +7,36 @@ import { useNavigate } from 'react-router-dom'
 
 function Home() {
 
-    const navigate=useNavigate()
+    const finalTitle = 'Miner'
+    const [visibleTitle, setVisibleTitle] = useState('')
 
-    const handleClick=(e)=>{
+    useEffect(() => {
+        let currIndex = 0;
+        let expanding = true;
+        const expandContract = setInterval(() => {
+            if (expanding) {
+                console.log("Hello",currIndex);
+                currIndex++
+                setVisibleTitle(finalTitle.slice(0, currIndex))
+                if (currIndex === finalTitle.length)
+                    expanding = false;
+            }
+            else {
+                currIndex--;
+                setVisibleTitle(finalTitle.slice(0, currIndex - 1))
+                if (currIndex === 0)
+                    {
+                        expanding=true;
+                        setVisibleTitle('M')
+                    }
+            }
+        }, 300)
+        return () => clearInterval(expandContract);
+    },[])
+
+    const navigate = useNavigate()
+
+    const handleClick = (e) => {
         e.preventDefault()
         navigate("/movies")
     }
@@ -18,16 +45,16 @@ function Home() {
         < div className='flex gap-10 flex-col-reverse lg:flex-row lg:justify-around min-h-screen 
         justify-center items-center text-center'>
             <section id='home-main' className='flex flex-col gap-4 justify-center max-w-screen-md px-3 '>
-                <motion.div
-                    initial={{ opacity: 0, x: -100 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 1.5 }}
-                >
-                    <article id='home-content' className='flex flex-col gap-8'>
-                        <h1 className='text-5xl text-center'>TITLE OF PROJECT!</h1>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem necessitatibus ducimus vero suscipit eius hic eum soluta in, dignissimos quibusdam laborum voluptatem totam iure veniam ad numquam inventore magnam provident vitae distinctio harum itaque architecto! Minima adipisci eum tenetur, itaque architecto quas incidunt ipsa ut!</p>
-                    </article>
-                </motion.div>
+                <article id='home-content' className='flex flex-col gap-8'>
+                    <h1 className='text-7xl text-center tracking-wider ' id='main-title'><span className='text-special'>Movie</span>{visibleTitle}</h1>
+                    <motion.div
+                        initial={{ opacity: 0, x: -100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 1.5 }}
+                    >
+                        <p>MovieMiner delves into the world of cinema to suggest movies similar to the one you have in mind. Enter a title, and we'll curate a personalized list of recommendations based on genre, themes, actors, directors, and more.</p>
+                    </motion.div>
+                </article>
                 <div className='btn-container'>
                     <button
                         id='home-btn'
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       boxShadow: {
         'glow-1': "0 0 10px rgba(50, 154, 166, 0.7), 0 0 20px rgba(50, 154, 166, 0.6),0 0 30px rgba(50, 154, 166, 0.5);"
+      },
+      textColor: {
+        'special': "rgb(50, 154, 166)"
       }
     },
   },
