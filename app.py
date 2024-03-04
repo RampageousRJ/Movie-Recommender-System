11af7f02edb374a72422db2d388a9f9df449f7c0
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2024-03-04 22:41:16 +0530

    Refactor Matrix Pickle Removed

--- app.py
+++ app.py
@@ -1,35 +1,15 @@
-from flask import Flask,request,render_template,url_for
+from flask import Flask,request,render_template
 from wtforms import StringField,SubmitField
 from flask_wtf import FlaskForm
-import pandas as pd
-import pickle
 import requests
+from model.matrix import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='supersecretspecialkey'
 
-f = open('model/dataframe.pkl','rb')
-df = pickle.load(f)
-f.close()
-
-fm = open('model/matrix.pkl','rb')
-similarity_matrix = pickle.load(fm)
-fm.close()
-
 class MovieForm(FlaskForm):
     title = StringField("Enter movie: ")
     submit = SubmitField("Recommend")
-
-def fetch(movie):
-    try:
-        index = df[df['Title_Lower']==movie.lower()].index[0]
-    except:
-        return "Not Available"
-    movies = sorted(list(enumerate(similarity_matrix[index])),reverse=True,key=lambda x:x[1])[1:7]
-    similar_movies = []
-    for movie in movies:
-        similar_movies.append(df.iloc[movie[0]])
-    return pd.DataFrame(similar_movies).sort_values(by='Popularity',ascending=False).drop(['Title_Lower','Overview_Stemmed'],axis=1).T.to_json()
 
 @app.route('/api',methods=['POST'])
 def api():
@@ -50,5 +30,5 @@ def home():
         return render_template('home.html',movies=movies,form=form)
     return render_template('home.html',form=form)
 
-# if __name__=='__main__':
-#     app.run(debug=1)
+if __name__=='__main__':
+    app.run(debug=1)
